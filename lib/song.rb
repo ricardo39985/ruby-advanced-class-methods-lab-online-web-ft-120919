719cde660dc1c966c43e42d1411294afa8a01158
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-20 20:31:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,7 @@ class Song
   end
   def self.new_from_filename(filename)
     song_name, song_artist = filename.split(" - ")[0]), filename.split(" - ")[1].chomp(".mp3")
+    create_by_name(song_name).artist_name = song_artist
     binding.pry
   end
   def save
