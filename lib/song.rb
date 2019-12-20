d3ac908ecaee29bcbd1b9390e7d273e08a32712f
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-20 20:16:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,39 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  def self.create
+    song = Song.new
+    all << song
+    song
 
+  end
   def self.all
     @@all
   end
+  def self.new_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    new_song
+  end
+  def self.create_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    all << new_song
+    new_song
+  end
+  def self.find_by_name(name)
+    all.find { |song| song.name==name }
+  end
+  def self.find_or_create_by_name(name)
+    if result=find_by_name(name)
+      result
+    else
+      create_by_name(name)
+    end
 
+  end
   def save
     self.class.all << self
-  end
-  def self.create
-    self.song.all << self
-    self
   end
 
 end
