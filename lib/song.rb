3bc6a28a20053b04c34fc2803b7e87c358083004
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-20 20:45:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,11 @@ class Song
     new_song, new_song.artist_name = create_by_name(song_name), song_artist
     new_song
   end
+  def self.create_from_filename(filename)
+    new_song = new_from_filename
+    new_song.save
 
+  end
   def save
     self.class.all << self
   end
